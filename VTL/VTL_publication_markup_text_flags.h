553561d9f461b756171b07bd566d129ef9e57001
Egor <54379419+MorgooN@users.noreply.github.com>
2025-05-31 22:23:30 +0300

    Update VTL_publication_markup_text_flags.h

--- VTL/VTL_publication_markup_text_flags.h
+++ VTL/VTL_publication_markup_text_flags.h
@@ -8,17 +8,17 @@ extern "C"
 
 #include <stdbool.h>
 
-typedef enum _VTL_publication_marked_text_markup_type
+typedef enum _VTL_publication_marked_text_MarkupType
 {
     VTL_publication_markup_type_kStandardMD = 0,
     VTL_publication_markup_type_kTelegramMD,
     VTL_publication_markup_type_kHTML,
     VTL_publication_markup_type_kBB
-} VTL_publication_marked_text_markup_type;
+} VTL_publication_marked_text_MarkupType;
 
-#define VTL_publication_marked_text_markup_type_max VTL_publication_markup_type_kBB
-#define VTL_publication_text_type_regular_shift (VTL_publication_marked_text_markup_type_max + 1)
-#define VTL_publication_text_type_max VTL_publication_text_type_regular_shift
+#define VTL_publication_marked_text_MarkupTypeMax VTL_publication_markup_type_kBB
+#define VTL_publication_text_TypeRegularShift (VTL_publication_marked_text_markup_type_max + 1)
+#define VTL_publication_text_TypeMax VTL_publication_text_type_regular_shift
 
 typedef int VTL_publication_marked_text_type_flags;
 
