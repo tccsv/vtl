5e55113948fd04c0619689b397315f05c419a5a1
Egor <54379419+MorgooN@users.noreply.github.com>
2025-05-31 22:25:47 +0300

    Update VTL_file_ops.h

--- VTL/VTL_file_ops.h
+++ VTL/VTL_file_ops.h
@@ -13,7 +13,7 @@ extern "C"
 понимаю это как интерфейс для работы с файлами
 */
 // структура с результатмси файловх операций
-typedef enum _VTL_publication_file_result
+typedef enum _VTL_publication_FileResult
 {
     VTL_publication_file_res_kOk = 0,
     VTL_publication_file_res_kErrorOpen,
@@ -21,15 +21,15 @@ typedef enum _VTL_publication_file_result
     VTL_publication_file_res_kErrorWrite,
     VTL_publication_file_res_kErrorClose,
     VTL_publication_file_res_kErrorMemory
-} VTL_publication_file_result;
+} VTL_publication_FileResult;
 
 // операции с файлами
-VTL_publication_file_result VTL_publication_FileReadS(const char* filename, char** content, size_t* size);
-VTL_publication_file_result VTL_publication_FileWriteS(const char* filename, const char* content, size_t size);
-VTL_publication_file_result VTL_publication_FileAppendS(const char* filename, const char* content, size_t size);
+VTL_publication_FileResult VTL_publication_FileReadS(const char* filename, char** content, size_t* size);
+VTL_publication_FileResult VTL_publication_FileWriteS(const char* filename, const char* content, size_t size);
+VTL_publication_FileResult VTL_publication_FileAppendS(const char* filename, const char* content, size_t size);
 
 // обработка ошибок
-const char* VTL_publication_FileGetErrorMessage(VTL_publication_file_result result);
+const char* VTL_publication_FileGetErrorMessage(VTL_publication_FileResult result);
 
 #ifdef __cplusplus
 }
